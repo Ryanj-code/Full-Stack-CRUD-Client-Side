144077db024f9a62922a5358506e0dc98986a0f5
Ryan Jiang <jiangr0421@gmail.com>
2023-05-13 21:38:22 -0400

    Revert "Image for home page"
    This reverts commit 91a44207a874194cb96972ecb45b8467b57bebca.

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -4,19 +4,11 @@ HomePageView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the home page.
 ================================================== */
-const imageSize = {
-  width: '500px',
-  height: '400px'
-}
-
-
 const HomePageView = () => {
-  const imageUrl = 'https://media.istockphoto.com/id/1319991057/photo/law-quadrangle-university-of-michigan-ann-arbor-aerial-view.jpg?b=1&s=170667a&w=0&k=20&c=4v-yToqfngkdp_lcgOBId11FGjbnAcTi6HQ3aCOfer4=';
   // Render Home page view
   return (
     <div >
       <h1>Home Page</h1>
-      <img src={imageUrl} alt="Home Page" style={imageSize}></img>
     </div>
   );    
 }
